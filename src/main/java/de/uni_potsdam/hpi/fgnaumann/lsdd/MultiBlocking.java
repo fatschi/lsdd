f0a0f1dda67cfdf58199108e30b42ad4c6ac6464
Fabian Tschirschnitz <fabian.tschirschnitz@student.hpi.uni-potsdam.de>
2013-05-22 16:07:02 +0200

    added simmetrics to pom and made fat-jar packaging, edited mr-code

--- src/main/java/de/uni_potsdam/hpi/fgnaumann/lsdd/MultiBlocking.java
+++ src/main/java/de/uni_potsdam/hpi/fgnaumann/lsdd/MultiBlocking.java
@@ -5,12 +5,11 @@
 package de.uni_potsdam.hpi.fgnaumann.lsdd;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
+import uk.ac.shef.wit.simmetrics.similaritymetrics.AbstractStringMetric;
+import uk.ac.shef.wit.simmetrics.similaritymetrics.Levenshtein;
 import eu.stratosphere.pact.common.contract.FileDataSink;
 import eu.stratosphere.pact.common.contract.FileDataSource;
 import eu.stratosphere.pact.common.contract.MapContract;
@@ -25,6 +24,7 @@ import eu.stratosphere.pact.common.stubs.Collector;
 import eu.stratosphere.pact.common.stubs.MapStub;
 import eu.stratosphere.pact.common.stubs.ReduceStub;
 import eu.stratosphere.pact.common.type.PactRecord;
+import eu.stratosphere.pact.common.type.base.PactDouble;
 import eu.stratosphere.pact.common.type.base.PactInteger;
 import eu.stratosphere.pact.common.type.base.PactString;
 import eu.stratosphere.pact.common.type.base.parser.DecimalTextIntParser;
@@ -38,21 +38,20 @@ import eu.stratosphere.pact.common.type.base.parser.VarLengthStringParser;
  * 
  */
 public class MultiBlocking implements PlanAssembler, PlanAssemblerDescription {
+	public static final int THRESHOLD = 250;
 
 	@Override
 	public Plan getPlan(final String... args) {
 		// parse program parameters
 		/*
-		 * 4 file:///home/fabian/lsdd/data/freedb_discs.csv
-		 * file:///home/fabian/lsdd/data/freedb_tracks.csv
-		 * file:///home/fabian/lsdd/out
+		 * 4 file:///home/fabian/lsdd/data/mini.csv file:///home/fabian/lsdd/data/freedb_tracks.csv file:///home/fabian/lsdd/out
 		 */
 		final int noSubtasks = (args.length > 0 ? Integer.parseInt(args[0]) : 1);
 		final String inputFileDiscs = (args.length > 1 ? args[1] : "");
 		final String inputFileTracks = (args.length > 2 ? args[2] : "");
 		final String output = (args.length > 3 ? args[3] : "");
 
-		// create DataSourceContract for Orders input
+		// create DataSourceContract for discs input
 		// disc_id;freedbdiscid;"artist_name";"disc_title";"genre_title";"disc_released";disc_tracks;disc_seconds;"disc_language"
 		// 7;727827;"Tenacious D";"Tenacious D";"Humour";"2001";19;2843;"eng"
 		FileDataSource discs = new FileDataSource(RecordInputFormat.class,
@@ -72,24 +71,26 @@ public class MultiBlocking implements PlanAssembler, PlanAssemblerDescription {
 		MapContract firstBlockingStepMapper = MapContract
 				.builder(FirstBlockingStep.class).input(discs)
 				.name("first blocking step").build();
-		ReduceContract coutStepReducer = new ReduceContract.Builder(
-				CountStep.class, PactString.class, 0)
+		ReduceContract countStepReducer = new ReduceContract.Builder(
+				CountStep.class, PactString.class, 9)
 				.input(firstBlockingStepMapper).name("count records step")
 				.build();
 		MapContract unbalancedBlockFilterMapper = MapContract
-				.builder(UnbalancedBlockFilterStep.class).input(coutStepReducer)
-				.name("filter unbalanced blocks step").build();
+				.builder(UnbalancedBlockFilterStep.class)
+				.input(countStepReducer).name("filter unbalanced blocks step")
+				.build();
 		MapContract balancedBlockFilterMapper = MapContract
-				.builder(BalancedBlockFilterStep.class).input(coutStepReducer)
+				.builder(BalancedBlockFilterStep.class).input(countStepReducer)
 				.name("filter balanced blocks step").build();
 		ReduceContract matchStepReducer = new ReduceContract.Builder(
-				MatchStep.class, PactString.class, 0)
+				MatchStep.class, PactString.class, 9)
 				.input(unbalancedBlockFilterMapper).name("match step").build();
 		FileDataSink out = new FileDataSink(RecordOutputFormat.class, output,
 				matchStepReducer, "Output");
 		RecordOutputFormat.configureRecordFormat(out).recordDelimiter('\n')
-				.fieldDelimiter(' ').lenient(true).field(PactString.class, 0)
-				.field(PactString.class, 1);
+				.fieldDelimiter(' ').lenient(true).field(PactInteger.class, 0)
+				.field(PactInteger.class, 1).field(PactDouble.class, 2)
+				.field(PactString.class, 3);
 
 		// assemble the PACT plan
 		Plan plan = new Plan(out, "MultiBlocking");
@@ -112,18 +113,19 @@ public class MultiBlocking implements PlanAssembler, PlanAssemblerDescription {
 	 * 
 	 */
 	public static class FirstBlockingStep extends MapStub {
-		// initialize reusable mutable objects
-		private final PactRecord outputRecord = new PactRecord();
 
 		@Override
 		public void map(PactRecord record, Collector<PactRecord> collector) {
-			PactString genre = record.getField(4, PactString.class);
-			PactString year = record.getField(5, PactString.class);
+			String genre = record.getField(4, PactString.class).getValue()
+					.replace("\"", "");
+			genre = genre.length() > 2 ? genre.substring(0, 2) : "";
+			String year = record.getField(5, PactString.class).getValue()
+					.replace("\"", "");
+			year = year.length() >= 4 ? year.substring(0, 3) : "";
 			PactString blockingKey = new PactString(genre);
 			AsciiUtils.toLowerCase(blockingKey);
-			outputRecord.setField(0, blockingKey);
-			outputRecord.setField(1, record.getField(2, PactString.class));
-			collector.collect(outputRecord);
+			record.setField(9, blockingKey);
+			collector.collect(record);
 		}
 	}
 
@@ -134,7 +136,6 @@ public class MultiBlocking implements PlanAssembler, PlanAssemblerDescription {
 	 * @author richard.meissner@student.hpi.uni-potsdam.de
 	 * 
 	 */
-	@Combinable
 	public static class MatchStep extends ReduceStub {
 		@Override
 		public void reduce(Iterator<PactRecord> records,
@@ -145,82 +146,43 @@ public class MultiBlocking implements PlanAssembler, PlanAssemblerDescription {
 				record = records.next();
 				r_temp.add(record);
 			}
+			System.out.println(r_temp.size());
 			for (int i = 0; i < r_temp.size(); i++) {
-				for (int j = i; j < r_temp.size(); j++) {
+				for (int j = 0; j < r_temp.size(); j++) {
 					PactRecord r1 = r_temp.get(i);
 					PactRecord r2 = r_temp.get(j);
-					if (jaccardSimilarity(r1.getField(1, PactString.class)
-							.getValue(), r2.getField(1, PactString.class)
-							.getValue()) > 0.8) {
+					AbstractStringMetric dist = new Levenshtein();
+					if (!(r1.getField(0, PactInteger.class).equals(r2.getField(
+							0, PactInteger.class)))) {
 						PactRecord outputRecord = new PactRecord();
 						outputRecord.setField(0,
-								r1.getField(0, PactString.class));
+								r1.getField(0, PactInteger.class));
 						outputRecord.setField(1,
-								r1.getField(1, PactString.class));
-						outputRecord.setField(1,
-								r2.getField(1, PactString.class));
+								r2.getField(0, PactInteger.class));
+						outputRecord.setField(
+								2,
+								new PactDouble(dist.getSimilarity(
+										r1.getField(3, PactString.class)
+												.getValue(),
+										r2.getField(3, PactString.class)
+												.getValue())));
+						outputRecord.setField(
+								3,
+								r1.getField(9, PactString.class));
 						out.collect(outputRecord);
 					}
 				}
 			}
 		}
 
-		/**
-		 * 
-		 * @see http://www.cs.rit.edu/~vvs1100/Code/JaccardSimilarity.java
-		 * @param similar1
-		 * @param similar2
-		 * @return jaccard index
-		 */
-		public static double jaccardSimilarity(String similar1, String similar2) {
-			HashSet<String> h1 = new HashSet<String>();
-			HashSet<String> h2 = new HashSet<String>();
-
-			for (String s : similar1.split("\\s+")) {
-				h1.add(s);
-			}
-			for (String s : similar2.split("\\s+")) {
-				h2.add(s);
-			}
-
-			int sizeh1 = h1.size();
-			// Retains all elements in h3 that are contained in h2 ie
-			// intersection
-			h1.retainAll(h2);
-			// h1 now contains the intersection of h1 and h2
-
-			h2.removeAll(h1);
-			// h2 now contains unique elements
-
-			// Union
-			int union = sizeh1 + h2.size();
-			int intersection = h1.size();
-
-			return (double) intersection / union;
-
-		}
-
-		/*
-		 * (non-Javadoc)
-		 * 
-		 * @see
-		 * eu.stratosphere.pact.common.stubs.ReduceStub#combine(java.util.Iterator
-		 * , eu.stratosphere.pact.common.stubs.Collector)
-		 */
-		@Override
-		public void combine(Iterator<PactRecord> records,
-				Collector<PactRecord> out) throws Exception {
-			// the logic is the same as in the reduce function, so simply call
-			// the reduce method
-			this.reduce(records, out);
-		}
 	}
 
 	/**
-	 * Reducer that countes the entries of each block to identify unbalanced
+	 * Reducer that counts the entries of each block to identify unbalanced
 	 * blocks
 	 * 
 	 * @author richard.meissner@student.hpi.uni-potsdam.de
+	 * @author fabian.tschirschnitz@student.hpi.uni-potsdam.de
 	 * 
 	 */
 	@Combinable
@@ -267,12 +229,10 @@ public class MultiBlocking implements PlanAssembler, PlanAssemblerDescription {
 	 * 
 	 */
 	public static class UnbalancedBlockFilterStep extends MapStub {
-		// initialize reusable mutable objects
-		public static int THRESHOLD = 250;
 
 		@Override
 		public void map(PactRecord record, Collector<PactRecord> collector) {
-			if (record.getField(record.getNumFields()-1, PactInteger.class)
+			if (record.getField(record.getNumFields() - 1, PactInteger.class)
 					.getValue() <= THRESHOLD)
 				collector.collect(record);
 		}
@@ -285,12 +245,10 @@ public class MultiBlocking implements PlanAssembler, PlanAssemblerDescription {
 	 * 
 	 */
 	public static class BalancedBlockFilterStep extends MapStub {
-		// initialize reusable mutable objects
-		public static int THRESHOLD = 250;
 
 		@Override
 		public void map(PactRecord record, Collector<PactRecord> collector) {
-			if (record.getField(record.getNumFields(), PactInteger.class)
+			if (record.getField(record.getNumFields() - 1, PactInteger.class)
 					.getValue() > THRESHOLD)
 				collector.collect(record);
 		}
